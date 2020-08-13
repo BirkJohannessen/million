7823f6069c719942b6fdaff1046fe1644ec64d78
Birk <birk.johannessen@hotmail.com>
2020-08-14 00:34:59 +0200

    kode endring i prosjekt

--- projectx/src/projectx/mproject.java
+++ projectx/src/projectx/mproject.java
@@ -4,13 +4,13 @@ public class mproject {
 
 	public static void main(String[] args) {
 		
-		// kommentar
+		// dette er en pissdårlig kode..
 		
 
 
 	for (int i = 1; i<=4; i++) 
 		System.out.println(i);
 	    System.out.println("you stupid, dumb shit, god dammed, motherfucker!");
-
+	    System.out.println("donkey kong, every day");
 }
 }
