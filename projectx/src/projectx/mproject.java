44269aec3722e9bfd53c354c9dfa0cdae9515ea5
BirkJohannessen <Birk.Johannessen@hotmail.com>
2020-08-14 11:01:21 +0200

    test

--- projectx/src/projectx/mproject.java
+++ projectx/src/projectx/mproject.java
@@ -4,13 +4,14 @@ public class mproject {
 
 	public static void main(String[] args) {
 		
-		// dette er en pissdårlig kode..
+		// dette er en pissdårlig kode.. nei det er det ikke
+		//gjør noe med det.test
 		
 
 
 	for (int i = 1; i<=4; i++) 
 		System.out.println(i);
 	    System.out.println("you stupid, dumb shit, god dammed, motherfucker!");
-	    System.out.println("donkey kong, every day");
+	    System.out.println("donkey kong, every day lala");
 }
 }
